ce0fa5561d7c166d1f0a0d2a9199eb7c8ac6da5e
Max Desyatov <max.desyatov@gmail.com>
2014-06-30 13:44:07 +0300

    Initial Commit

---
+++ Wyrd/Wyrd.h
@@ -0,0 +1,19 @@
+//
+//  Wyrd.h
+//  Wyrd
+//
+//  Created by Max Desyatov on 30/06/2014.
+//  Copyright (c) 2014 Max Desyatov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Wyrd.
+FOUNDATION_EXPORT double WyrdVersionNumber;
+
+//! Project version string for Wyrd.
+FOUNDATION_EXPORT const unsigned char WyrdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Wyrd/PublicHeader.h>
+
+
---
+++ WyrdTests/WyrdTests.swift
@@ -0,0 +1,35 @@
+//
+//  WyrdTests.swift
+//  WyrdTests
+//
+//  Created by Max Desyatov on 30/06/2014.
+//  Copyright (c) 2014 Max Desyatov. All rights reserved.
+//
+
+import XCTest
+
+class WyrdTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
